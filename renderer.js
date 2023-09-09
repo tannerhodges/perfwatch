40b5fca68993349f9f7ef49c6dd8e80cb4747508
Tanner Hodges <me@tannerhodges.com>
2023-09-09 10:28:15 -0400

    Local files

--- renderer.js
+++ renderer.js
@@ -1,1 +1,24 @@
-// 🙈
+document.addEventListener('alpine:init', () => {
+	Alpine.store('data', {
+		events: [],
+		fileChanges: [],
+		folderPath: '',
+	});
+});
+
+document.addEventListener('alpine:initialized', () => {
+	Alpine.store('data', {
+		events: Alpine.$persist([]).as('events'),
+		fileChanges: Alpine.$persist([]).as('fileChanges'),
+		folderPath: Alpine.$persist('').as('folderPath'),
+	});
+});
+
+window.electronAPI.handleLog((data) => {
+	Alpine.store('data').events.push(data);
+});
+
+window.electronAPI.handleFileChange((event, path) => {
+	const timestamp = performance.timeOrigin + performance.now();
+	Alpine.store('data').fileChanges.push({ timestamp, event, path });
+});

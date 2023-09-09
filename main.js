86cfad905253e2e07efd58294bc1d31262051f80
Tanner Hodges <me@tannerhodges.com>
2023-09-09 15:29:17 -0400

    Ignore hidden folders (like .git/) in file watcher

--- main.js
+++ main.js
@@ -70,6 +70,7 @@ async function resetFileWatcher() {
 	fileWatcher = chokidar.watch([], {
 		ignored: [
 			'**/.*', // Ignore hidden files
+			'**/.*/**/*', // Ignore hidden folders
 			'**/node_modules/**/*',
 		],
 		ignoreInitial: true,

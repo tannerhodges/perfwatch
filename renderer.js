c1bd81e0b9eda1a5f8de420fd4347890e8c6c2ea
Tanner Hodges <me@tannerhodges.com>
2023-09-09 09:15:56 -0400

    Add file changes

--- renderer.js
+++ renderer.js
@@ -1,12 +1,1 @@
-// const folderButtonEl = document.getElementById('folder-button');
-// const folderPathEl = document.getElementById('folder-path');
-// const fileChangesEl = document.getElementById('file-changes');
-
-// folderButtonEl.addEventListener('click', async () => {
-// 	const folderPath = await window.electronAPI.openFolder();
-// 	folderPathEl.innerText = folderPath;
-// });
-
-// window.electronAPI.handleFileChange((event, path) => {
-// 	fileChangesEl.insertAdjacentHTML('beforeend', `<li>${event}: ${path}</li>`);
-// });
+// 🙈

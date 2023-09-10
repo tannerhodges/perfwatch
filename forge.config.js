593ec3e966de4e5bfaa198e8c3a95eedcc9cd387
Tanner Hodges <me@tannerhodges.com>
2023-09-09 23:06:15 -0400

    Fix code signing

--- forge.config.js
+++ forge.config.js
@@ -4,30 +4,39 @@ module.exports = {
 	// https://www.electronforge.io/guides/code-signing/code-signing-macos
 	// https://stackoverflow.com/questions/46480682/how-to-sign-electron-app-using-electron-forge
 	packagerConfig: {
+		ignore: [
+			'/_tanner_misc',
+			'/images',
+			'\.*\.code-',
+			'\.editorconfig',
+			'\.env',
+			'\.env',
+			'\.gitignore',
+			'\.nvmrc',
+			'example.html',
+		],
+		// https://github.com/electron/osx-sign
 		osxSign: {
 			identity: process.env.APPLE_IDENTITY,
+			entitlements: true,
+			hardenedRuntime: true,
+			// https://github.com/electron/notarize/issues/54#issuecomment-704625356
+			'gatekeeper-assess': false,
+		},
+		// https://github.com/electron/notarize
+		osxNotarize: {
+			tool: 'notarytool',
+			appleId: process.env.APPLE_ID,
+			appleIdPassword: process.env.APPLE_PASSWORD,
+			teamId: process.env.APPLE_TEAM_ID,
 		},
 	},
 	makers: [
 		{
-			name: "@electron-forge/maker-squirrel",
-			config: {
-				name: "perfwatch",
-			},
-		},
-		{
-			name: "@electron-forge/maker-zip",
+			name: '@electron-forge/maker-zip',
 			platforms: [
-				"darwin",
+				'darwin',
 			],
-		},
-		{
-			name: "@electron-forge/maker-deb",
-			config: {},
-		},
-		{
-			name: "@electron-forge/maker-rpm",
-			config: {},
 		},
 	],
 };
